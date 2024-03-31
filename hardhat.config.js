4e62f96e79bef8823170e0e40c716944af72398f
Riteek Rakesh <riteek7.12@gmail.com>
2024-03-31 15:01:19 +0530

    hardhat modifications

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 const fs = require('fs');
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -18,8 +18,8 @@ module.exports = {
       chainId: 1337
     },
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/pTuCuemkINLjIARnhNgnpCq88nUSCGfk",
-      accounts: [ "6c96d1da0bf690ba8824405e17b2e0b23f1b10f6e75de83be21b5c09c157916b" ]
+      url: process.env.REACT_APP_ALCHEMY_URL,
+      accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
     }
   },
   solidity: {
